359c4e44ec77a9e315311877351439445f6dca8e
Lorenzo Buitizon <the.keikun@gmail.com>
2026-09-20 10:18:00 +0800

    Add NPC script log viewer

--- lang/en_us.php
+++ lang/en_us.php
@@ -551,6 +551,15 @@ return array(
 	'BranchLogCharacterLabel' => 'Character',
 	'BranchLogMapLabel'       => 'Map',
 
+	'NpcLogTitle'             => 'List NPC Log',
+	'NpcLogHeading'           => 'NPC Log',
+	'NpcLogNotFound'          => 'No NPC logs found.',
+	'NpcLogDateLabel'         => 'Date/Time',
+	'NpcLogAccountLabel'      => 'Account',
+	'NpcLogCharacterLabel'    => 'Character',
+	'NpcLogMapLabel'          => 'Map',
+	'NpcLogMessageLabel'      => 'Message',
+
 	// Module: mail
 	// - mail/index
 	'MailerTitle'             => 'Form Mailer',
